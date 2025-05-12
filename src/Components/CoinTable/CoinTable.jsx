561ae8ee75a59a0952abfcd99c956947f8c81df6
krishna kunal <krishnakunal0704@gmail.com>
2025-05-13 03:09:05 +0530

    pagination

--- src/Components/CoinTable/CoinTable.jsx
+++ src/Components/CoinTable/CoinTable.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import { fetchCoinData } from "../../Services/fetchCoinData";
+import { useQuery } from "@tanstack/react-query";
+
 function CoinTable() {
-  return <div>CoinTable</div>;
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["coins", page],
+    queryFn: () => fetchCoinData(page, "usd"),
+    queries: {
+      retry: 2,
+      retryDelay: 1000,
+      cacheTime: 1000 * 60 * 2,
+    },
+  });
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <>
+      CoinTable
+      <button onClick={() => setPage(page + 1)}>Click</button>
+      {page}
+    </>
+  );
 }
 
 export default CoinTable;
