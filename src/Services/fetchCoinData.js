dafebadb78d20557838240a3d5a44a843db74089
krishna kunal <krishnakunal0704@gmail.com>
2025-05-15 01:21:26 +0530

    Added coin table ui with cache

--- src/Services/fetchCoinData.js
+++ src/Services/fetchCoinData.js
@@ -6,8 +6,8 @@ export async function fetchCoinData(page = 1, currency = "usd") {
     const response = await axiosInstance.get(
       `/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=${page}`
     );
-    console.log(response);
-    return response;
+
+    return response.data;
   } catch (error) {
     console.log(error);
     return null;
